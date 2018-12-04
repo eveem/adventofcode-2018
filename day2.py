c4c17d090ea6ff857ea3c101ce46950db11b0616
eveem <monpriya.tammavong@gmail.com>
2018-12-04 16:38:03 +0700

    finish day 2

---
+++ day2.py
@@ -0,0 +1,30 @@
+# first star
+
+# file = open('2_input.txt')
+
+# three = 0
+# two = 0
+
+# for line in file:
+# 	d = {}
+# 	for c in line:
+# 		if c in d:
+# 			d[c] += 1
+# 		else:
+# 			d[c] = 1
+# 	dthree = False
+# 	dtwo = False
+# 	for i in d:
+# 		if d[i] == 3 and dthree == False:
+# 			three += 1
+# 			dthree = True
+# 		if d[i] == 2 and dtwo == False:
+# 			two += 1
+# 			dtwo = True
+
+# print(three * two)
+
+# second star
+
+import nltk
+
